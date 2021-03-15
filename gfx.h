93c1d487c4b9ac22436ebc7bf393037fb831f2eb
Peter Ivanov <ivanovp@gmail.com>
2021-03-15 19:36:54 +0100

    Code cleanup.

--- gfx.h
+++ gfx.h
@@ -23,13 +23,6 @@
 #define TEXT_X(x)       (TEXT_X_0 + FONT_NORMAL_SIZE_X_PX * (x))
 #define TEXT_Y(y)       (TEXT_Y_0 + (FONT_NORMAL_SIZE_Y_PX) * (y))
 
-#ifndef DATA_DIR
-#define DATA_DIR                "."
-#endif
-#define GFX_DIR                 DATA_DIR "/gfx/"
-#define BACKGROUND_PNG          GFX_DIR "bg.png"
-
-#define gfx_color_rgb(r,g,b)                    ( ( r << 24 ) | ( g << 16 ) | ( b << 8 ) | 0xFF )
 #define gfx_line_draw(x1, y1, x2, y2)           lineRGBA(screen, x1, y1, x2, y2, config.text_color.r, config.text_color.g, config.text_color.b, 0xFF)
 #define gfx_font_print(x,y,s)                   stringRGBA(screen, x, y, s, config.text_color.r, config.text_color.g, config.text_color.b, 0xFF)
 #define gfx_font_print_fromright(x,y,s)         stringRGBA(screen, x - strlen(s) * FONT_NORMAL_SIZE_X_PX, y, s, config.text_color.r, config.text_color.g, config.text_color.b, 0xFF)
